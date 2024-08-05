be1a4f0dae587a8630801577ef153cd748cc510e
MXWJ <z2116344865@gmail.com>
2024-08-05 21:03:48 +0800

    配置Gateway路由网关和路由过滤器

--- book-service/src/main/java/com/test/controller/BookController.java
+++ book-service/src/main/java/com/test/controller/BookController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class BookController {
@@ -15,7 +17,10 @@ public class BookController {
     BookService service;
 
     @RequestMapping("/book/{bid}")
-    Book findBookById(@PathVariable("bid") int bid){
+    Book findBookById(@PathVariable("bid") int bid,
+                      HttpServletRequest request ){
+        System.out.println(request.getHeader("Test"));
+
         return service.getBookById(bid);
     }
 }
